e0c4eb769eeb2ef706ee9f2a635dd1939b34cf91
Narin Laboratory <halo@tutorkeren.com>
2017-09-15 20:17:28 +0800

    Initial Firmware
    Initial firmare release (Arduino .INO)

---
+++ GovindaRC-Ino/GovindaRC-Ino.ino
@@ -0,0 +1,290 @@
+//MIT License
+//
+//Copyright (c) 2017 Narin Laboratory
+//
+//Permission is hereby granted, free of charge, to any person obtaining a copy
+//of this software and associated documentation files (the "Software"), to deal
+//in the Software without restriction, including without limitation the rights
+//to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+//copies of the Software, and to permit persons to whom the Software is
+//furnished to do so, subject to the following conditions:
+//
+//The above copyright notice and this permission notice shall be included in all
+//copies or substantial portions of the Software.
+//
+//THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+//IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+//FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+//AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+//LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+//OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+//SOFTWARE.
+  
+#include <ArduinoJson.h>
+#include <ESP8266WiFi.h>
+#include <WiFiClient.h>
+#include <ESP8266WebServer.h>
+#include <WebSocketsServer.h>
+#include <Hash.h>
+#include <Servo.h> 
+
+const char* WIFI_SSID     = "AndroidAP";
+const char* WIFI_PASSWORD = "ingattuhan";
+
+#define PIN_PWMA 5
+#define PIN_PWMB 4
+#define PIN_AIN2 15
+#define PIN_AIN1 0
+#define PIN_BIN2 2 //D4
+#define PIN_BIN1 14 //D5
+#define PIN_LIGHT 16
+#define PIN_SERVOX 12
+#define PIN_SERVOY 13
+#define PIN_ECHO 3
+#define PIN_TRIG 1
+  
+#define PAYLOAD_SIZE 256
+#define SERVOX_CAL 30 // + if too left, - if too right
+#define SERVOY_CAL 30 // + if too up, - if too down
+#define ULTRASONIC_MAX 300
+#define ULTRASONIC_MIN 3
+#define ULTRASONIC_SAMPLE 3
+WebSocketsServer webSocket = WebSocketsServer(81);
+Servo SERVOX;
+Servo SERVOY;
+
+// Function Prototype
+float scan(int sample = ULTRASONIC_SAMPLE);
+//GLOBAL Variable (flag)
+int _PWM_A, _PWM_B, _SERVOX, _SERVOY, _DIRECTION;
+
+void speed(int pwm_a, int pwm_b){
+    if(pwm_a > 1023){
+        pwm_a = 1023;
+    }
+    else if(pwm_a < 0){
+        pwm_a = 0;
+    }
+    if(pwm_b > 1023){
+        pwm_b = 1023;
+    }
+    else if(pwm_b < 0){
+        pwm_b = 0;
+    }
+    _PWM_A = pwm_a;
+    _PWM_B = pwm_b;
+    analogWrite(PIN_PWMA, pwm_a);
+    analogWrite(PIN_PWMB, pwm_b);
+}
+
+void navigate(int direction) {
+    // 0 =  stop, 1 = forward, 2 = backward, 3 = left, 4 = rights
+    if ( direction == 1 ) {
+        if(_DIRECTION != 1 && _PWM_A > 256 && _PWM_B > 256){
+            // Loosing gas
+            int pwm_a = _PWM_A;
+            int pwm_b = _PWM_B;
+            speed(256,256);
+            delay(250);
+            digitalWrite(PIN_AIN1, LOW);
+            digitalWrite(PIN_AIN2, HIGH);
+            digitalWrite(PIN_BIN1, LOW);
+            digitalWrite(PIN_BIN2, HIGH);
+            delay(250);
+            // Restoring gas
+            speed(pwm_a, pwm_b);
+        }
+        else{
+            digitalWrite(PIN_AIN1, LOW);
+            digitalWrite(PIN_AIN2, HIGH);
+            digitalWrite(PIN_BIN1, LOW);
+            digitalWrite(PIN_BIN2, HIGH);
+        }
+
+        _DIRECTION = 1;
+    }
+    else if ( direction == 2 ) {
+        if(_DIRECTION != 2 && _PWM_A > 256 && _PWM_B > 256){
+            // Loosing gas
+            int pwm_a = _PWM_A;
+            int pwm_b = _PWM_B;
+            speed(256,256);
+            delay(250);
+            digitalWrite(PIN_AIN1, HIGH);
+            digitalWrite(PIN_AIN2, LOW);
+            digitalWrite(PIN_BIN1, HIGH);
+            digitalWrite(PIN_BIN2, LOW);
+            delay(250);
+            // Restoring gas
+            speed(pwm_a, pwm_b);
+        }
+        else{
+            digitalWrite(PIN_AIN1, HIGH);
+            digitalWrite(PIN_AIN2, LOW);
+            digitalWrite(PIN_BIN1, HIGH);
+            digitalWrite(PIN_BIN2, LOW);
+        }
+
+        _DIRECTION = 2;
+    }
+    else if ( direction == 3 ) {
+        if(_DIRECTION != 3 && _PWM_A > 256 && _PWM_B > 256){
+            // Loosing gas
+            int pwm_a = _PWM_A;
+            int pwm_b = _PWM_B;
+            speed(256,256);
+            delay(250);
+            digitalWrite(PIN_AIN1, HIGH);
+            digitalWrite(PIN_AIN2, LOW);
+            digitalWrite(PIN_BIN1, LOW);
+            digitalWrite(PIN_BIN2, HIGH);
+            delay(250);
+            // Restoring gas
+            speed(pwm_a, pwm_b);
+        }
+        else{
+            digitalWrite(PIN_AIN1, HIGH);
+            digitalWrite(PIN_AIN2, LOW);
+            digitalWrite(PIN_BIN1, LOW);
+            digitalWrite(PIN_BIN2, HIGH);
+        }
+
+        _DIRECTION = 3;
+    }
+    else if ( direction == 4 ) {
+        if(_DIRECTION != 4 && _PWM_A > 256 && _PWM_B > 256){
+            // Loosing gas
+            int pwm_a = _PWM_A;
+            int pwm_b = _PWM_B;
+            speed(256,256);
+            delay(250);
+            digitalWrite(PIN_AIN1, LOW);
+            digitalWrite(PIN_AIN2, HIGH);
+            digitalWrite(PIN_BIN1, HIGH);
+            digitalWrite(PIN_BIN2, LOW);
+            delay(250);
+            // Restoring gas
+            speed(pwm_a, pwm_b);
+        }
+        else{
+            digitalWrite(PIN_AIN1, LOW);
+            digitalWrite(PIN_AIN2, HIGH);
+            digitalWrite(PIN_BIN1, HIGH);
+            digitalWrite(PIN_BIN2, LOW);
+        }
+
+        _DIRECTION = 4;
+    }
+    else {
+        digitalWrite(PIN_AIN1, LOW);
+        digitalWrite(PIN_AIN2, LOW);
+        digitalWrite(PIN_BIN1, LOW);
+        digitalWrite(PIN_BIN2, LOW);
+
+        _DIRECTION = 0;
+    }
+}
+
+void light(int state, int hold){
+    if(hold > 0){
+        digitalWrite(PIN_LIGHT, state);
+        delay(hold);
+        digitalWrite(PIN_LIGHT, !state);
+    }
+    else{
+        digitalWrite(PIN_LIGHT, state);
+    }
+}
+
+void look(int x, int y) {
+    if(x > 180){
+        x = 180 - SERVOX_CAL;
+    }
+    else if(x < 0){
+        x = 0 + SERVOX_CAL;
+    }
+    else{
+        x = x - SERVOX_CAL;
+    }
+    if(y > 180){
+        y = 180 - SERVOY_CAL;
+    }
+    else if(y < 0){
+        y = 0  + SERVOY_CAL;
+    }
+    else{
+        y = y - SERVOY_CAL;
+    }
+    
+    SERVOX.write(x);                
+    SERVOY.write(y);
+    _SERVOX = x;
+    _SERVOY = y;
+}
+
+float scan(int sample){
+    float cms = 0;
+    
+    for(int i = 0; i < sample; i++) {
+        digitalWrite(PIN_TRIG, LOW);
+        delayMicroseconds(2);
+        digitalWrite(PIN_TRIG, HIGH);
+        delayMicroseconds(10);
+        digitalWrite(PIN_TRIG, LOW);
+        long duration = pulseIn(PIN_ECHO, HIGH);
+        int cm = duration / 29 / 2;
+        if(cm > 300){
+            cm = ULTRASONIC_MAX;
+        }
+        else if(cm < 3){
+            cm = ULTRASONIC_MIN;
+        }
+        cms += cm;
+        delay(5);
+    }
+    
+    return cms / sample;
+}
+
+void setup() {
+  Serial.end();
+  //Lighting LED
+  pinMode(PIN_LIGHT, OUTPUT);
+  //Ultrasonic ranger
+  pinMode(PIN_TRIG, OUTPUT);
+  pinMode(PIN_ECHO, INPUT);
+  //Motor A
+  pinMode(PIN_PWMA, OUTPUT);
+  pinMode(PIN_AIN1, OUTPUT);
+  pinMode(PIN_AIN2, OUTPUT);
+  //Motor B
+  pinMode(PIN_PWMB, OUTPUT);
+  pinMode(PIN_BIN1, OUTPUT);
+  pinMode(PIN_BIN2, OUTPUT);
+  //Servo
+  pinMode(PIN_SERVOX, OUTPUT);
+  pinMode(PIN_SERVOY, OUTPUT);
+  SERVOX.attach(PIN_SERVOX);
+  SERVOY.attach(PIN_SERVOY);
+
+  //Serial.begin(115200);  
+  delay(10);
+    
+  WiFi.mode(WIFI_STA);
+  WiFi.begin(WIFI_SSID, WIFI_PASSWORD);
+  
+  //Serial.println("Trying to connect.");
+  while (WiFi.status() != WL_CONNECTED) {
+    light(1, 100);
+    delay(100);
+  }
+  //Serial.println("Connected!");
+  webSocket.begin();
+  webSocket.onEvent(webSocketEvent);
+  look(90,90);
+}
+  
+void loop() {
+    webSocket.loop();
+}
+
---
+++ GovindaRC-Ino/Router.ino
@@ -0,0 +1,180 @@
+void webSocketEvent(uint8_t num, WStype_t type, uint8_t * payload, size_t length) {
+    switch(type) {
+        case WStype_DISCONNECTED:
+            // Dim light
+            for (int i = 0; i < 3; i++){
+                light(1, 500);
+                delay(500);
+            }
+            break;
+        case WStype_CONNECTED:
+            {
+                IPAddress ip = webSocket.remoteIP(num);
+                // Dim light
+                light(1, 100);
+            }
+            break;
+        case WStype_TEXT:
+            {
+                // Command router
+                StaticJsonBuffer<PAYLOAD_SIZE> msg;
+                JsonObject& data = msg.parseObject(payload);
+
+                StaticJsonBuffer<PAYLOAD_SIZE> jsonBuffer;
+                JsonObject& root = jsonBuffer.createObject();
+                char payload[PAYLOAD_SIZE];
+                // Check message format
+                if(!data.success()){
+                    root["msgId"] = "-";
+                    root["status"] = 400;
+                    root["type"] = "debug";
+                    root["data"] = "-";
+                    root["desc"] = "Bad Request";
+                    root.printTo(payload, sizeof(payload));
+                    webSocket.sendTXT(num, payload);
+                    return;
+                }
+                // movement section
+                if(data["cmd"] == "move"){
+                    if(data["arg"] >= 0 && data["arg"] <= 4){
+                        navigate(data["arg"]);
+                        root["msgId"] = data["msgId"];
+                        root["status"] = 200;
+                        root["type"] = data["cmd"];
+                        root["data"] = data["arg"];
+                        root["desc"] = "OK";
+                        root.printTo(payload, sizeof(payload));
+                        webSocket.sendTXT(num, payload);
+                    }
+                    else{
+                        root["msgId"] = data["msgId"];
+                        root["status"] = 400;
+                        root["type"] = "debug";
+                        root["data"] = "move[arg: (int) 0-4]";
+                        root["desc"] = "Bad Request";
+                        root.printTo(payload, sizeof(payload));
+                        webSocket.sendTXT(num, payload);
+                    }
+                }
+                // head direction section
+                else if(data["cmd"] == "look"){
+                    if(data["arg"]["x"] >= 0 && data["arg"]["x"] <= 180 && 
+                        data["arg"]["y"] >= 0 && data["arg"]["y"] <= 180){
+                        look(data["arg"]["x"], data["arg"]["y"]);
+                        root["msgId"] = data["msgId"];
+                        root["status"] = 200;
+                        root["type"] = data["cmd"];
+                        root["data"] = data["arg"];
+                        root["desc"] = "OK";
+                        root.printTo(payload, sizeof(payload));
+                        webSocket.sendTXT(num, payload);
+                    }
+                    else{
+                        root["msgId"] = data["msgId"];
+                        root["status"] = 400;
+                        root["type"] = "debug";
+                        root["data"] = "look[ arg: (arr) {x: 0-180, y: 0-180} ]";
+                        root["desc"] = "Bad Request";
+                        root.printTo(payload, sizeof(payload));
+                        webSocket.sendTXT(num, payload);
+                    }
+                }
+                // Ultrasonic pinger section
+                else if(data["cmd"] == "scan"){
+                    if(data["arg"] > 0){
+                        float cm = scan(data["arg"]);
+                        root["msgId"] = data["msgId"];
+                        root["status"] = 200;
+                        root["type"] = data["cmd"];
+                        root["data"] = cm;
+                        root["desc"] = "OK";
+                        root.printTo(payload, sizeof(payload));
+                        webSocket.sendTXT(num, payload);
+                    }
+                    else{
+                        root["msgId"] = data["msgId"];
+                        root["status"] = 400;
+                        root["type"] = "debug";
+                        root["data"] = "scan[ arg: (int) 1-65535 ]";
+                        root["desc"] = "Bad Request";
+                        root.printTo(payload, sizeof(payload));
+                        webSocket.sendTXT(num, payload);
+                    }
+                }
+                // Light lamp section
+                else if(data["cmd"] == "light"){
+                    if(data["arg"] >= 0 && data["arg"] <= 1){
+                        light(data["arg"], 0);
+                        root["msgId"] = data["msgId"];
+                        root["status"] = 200;
+                        root["type"] = data["cmd"];
+                        root["data"] = data["arg"];
+                        root["desc"] = "OK";
+                        root.printTo(payload, sizeof(payload));
+                        webSocket.sendTXT(num, payload);
+                    }
+                    else{
+                        root["msgId"] = data["msgId"];
+                        root["status"] = 400;
+                        root["type"] = "debug";
+                        root["data"] = "light[ arg: (int) 0-1 ]";
+                        root["desc"] = "Bad Request";
+                        root.printTo(payload, sizeof(payload));
+                        webSocket.sendTXT(num, payload);
+                    }
+                }  
+                // speed section
+                else if(data["cmd"] == "speed"){
+                    if(data["arg"]["a"] >= 0 && data["arg"]["a"] <= 1023 && 
+                        data["arg"]["b"] >= 0 && data["arg"]["b"] <= 1023){
+                        speed(data["arg"]["a"], data["arg"]["b"]);
+                        root["msgId"] = data["msgId"];
+                        root["status"] = 200;
+                        root["type"] = data["cmd"];
+                        root["data"] = data["arg"];
+                        root["desc"] = "OK";
+                        root.printTo(payload, sizeof(payload));
+                        webSocket.sendTXT(num, payload);
+                    }
+                    else{
+                        root["msgId"] = data["msgId"];
+                        root["status"] = 400;
+                        root["type"] = "debug";
+                        root["data"] = "look[ arg: (arr) {a: 0-1023, b: 0-1023} ]";
+                        root["desc"] = "Bad Request";
+                        root.printTo(payload, sizeof(payload));
+                        webSocket.sendTXT(num, payload);
+                    }
+                }
+                // Not implemented
+                else{
+                    root["msgId"] = data["msgId"];
+                    root["status"] = 501;
+                    root["type"] = data["cmd"];
+                    root["data"] = data["arg"];
+                    root["desc"] = "Not Implemented";
+                    root.printTo(payload, sizeof(payload));
+                    webSocket.sendTXT(num, payload);
+                }  
+
+                //Serial.printf("[%u] get Text: %s\n", num, payload);
+
+                // send message to client
+                // webSocket.sendTXT(num, payload);
+
+                // send data to all connected clients
+                // webSocket.broadcastTXT("message here");
+            }
+            break;
+        case WStype_BIN:
+            {
+            //Serial.printf("[%u] get binary length: %u\n", num, length);
+            hexdump(payload, length);
+
+            // send message to client
+            // webSocket.sendBIN(num, payload, length);
+            }
+            break;
+    }
+
+}
